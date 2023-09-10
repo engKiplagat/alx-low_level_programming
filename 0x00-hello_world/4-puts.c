7f157c54011629f25aec366c6456643f9acf9c3a
engKiplagat <kiplagatharry5@gmail.com>
2023-09-10 08:21:34 -0700

    low level programming is cool

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main is a program that prints a line with puts functin
+ * returns 0 means it is a success
+ */
+int main(void)
+{
+puts("\*Programming is like building a multilingual puzzle");/*places a value */
+return (0);/*It should give a 0
+	     */
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the size of various computer types
+ * return 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printf("size of char:%lu byte(s)\n",(unsigned long)sizeof(a))
+	 printf("size of int :%lu byte(s)\n",(unsigned long)sizeof(b))
+	  printf("size of long int :%lu byte(s)\n",(unsigned long)sizeof(c))
+	  printf("size of long long:%lu byte(s)\n",(unsigned long)sizeof(d))
+	   printf("size of float :%lu byte(s)\n",(unsigned long)sizeof(f))
+	   return (0)
