0af1e3e2ec34d340be264eb2023805b53447b86c
engKiplagat <kiplagatharry5@gmail.com>
2023-09-12 03:08:05 -0700

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	string _putchar;
+
+	while _putchar < 3;
+
+	i++;
+	{
+		printf("\n");
+	}
+	printf ("_putchar\n");
+	}
+	char = _putchar();
+	return (0);
+}
